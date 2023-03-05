b13815c11e298972d5003e5fa752f76d74c2c100
Gui ximiti <gui.m.schmidt@gmail.com>
2023-03-05 15:09:59 -0300

    final commit

--- weather-app/modelo-webpack/src/main.js
+++ weather-app/modelo-webpack/src/main.js
@@ -1,29 +1,84 @@
-import variables, { erro404, nome, search, weatherBox, weatherDetails } from './modules/variables'
+
 
 import './assets/css/style.css';
 
+
+
+
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
 search.addEventListener('click', () => {
 
-    const APIKey = '08f03ea51287611aec367c0b824a48dd' ;
+    const APIKey = '08f03ea51287611aec367c0b824a48dd';
     const city = document.querySelector('.search-box input').value;
 
-    if (city === '') return;
-    
+    if (city === '')
+        return;
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather${city}&units=metric&appid=${APIKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
         .then(response => response.json())
         .then(json => {
-            if (json.cod === '404'){
+
+            if (json.cod === '404') {
                 container.style.height = '400px';
                 weatherBox.style.display = 'none';
-                weatherDetails.style.display = 'none'
-                erro404.style.display ='block';
-                erro404.classList.add('fadeIn')
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
                 return;
             }
-            erro404.style.display ='none';
-            erro404.classList.remove('fadeIn')
-            
-        })
 
-})
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'assets/img/clear.png';
+                    break;
+
+                case 'Rain':
+                    image.src = 'assets/img/rain.png';
+                    break;
+
+                case 'Snow':
+                    image.src = 'assets/img/snow.png';
+                    break;
+
+                case 'Clouds':
+                    image.src = 'assets/img/cloud.png';
+                    break;
+
+                case 'Haze':
+                    image.src = 'assets/img/mist.png';
+                    break;
+
+                default:
+                    image.src =  '';
+            }
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '590px';
+
+
+        });
+
+
+});
--- weather-app/modelo-webpack/src/modules/variables.js
+++ weather-app/modelo-webpack/src/modules/variables.js
@@ -3,6 +3,3 @@ export const search = document.querySelector('.search-box button')
 export const weatherBox = document.querySelector('.weather-box')
 export const weatherDetails = document.querySelector('.weather-details')
 export const erro404 = document.querySelector('.not-found');
-export const image = document.querySelector('.weather-box img')
-export const temperature = document.querySelector('weather-box .temperature')
-export const description = document.querySelector('.weather-box .description')
